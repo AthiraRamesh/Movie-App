5a2aa536b3d5410bc1596ac2ac2b6f9c67452c3a
AthiraRamesh <athirarameshm@gmail.com>
2023-09-16 11:58:05 +0530

    Fetching data from API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tmdb_api/tmdb_api.dart';
 
 void main() => runApp(const MyApp());
 
@@ -27,10 +28,42 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
+  void initState() {
+    loadmovies();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  List trendingmovies = [];
+  List topratedmovies = [];
+  List tv = [];
+  final String apikey = '88091677a5e745697a36a2106589fd0b';
+  final readaccesstoken =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ODA5MTY3N2E1ZTc0NTY5N2EzNmEyMTA2NTg5ZmQwYiIsInN1YiI6IjY0ZjMwZDBiMWYzMzE5MDEwMGE0ZTY2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lSBYVWbb1keexFWzoZFwIEf8oShSD44Zw0gOTJpYdQs';
+
+  loadmovies() async {
+    // initialize tmdb object
+    // ie, initialize apikey and readaccesstoken
+    TMDB tmdbWithCustomLogs = TMDB(ApiKeys(apikey, readaccesstoken),
+        logConfig: const ConfigLogger(showLogs: true, showErrorLogs: true));
+    Map trendingresult = await tmdbWithCustomLogs.v3.trending.getTrending();
+    Map topratedresult = await tmdbWithCustomLogs.v3.movies.getTopRated();
+    Map tvresult = await tmdbWithCustomLogs.v3.tv.getPopular();
+
+    //print(trendingresult);
+    setState(() {
+      trendingmovies = trendingresult['results'];
+      topratedmovies = topratedresult['results'];
+      tv = tvresult['results'];
+    });
+    print(trendingmovies);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Movie App'),
+        title: const Text('Flutter Movie App'),
       ),
     );
   }
